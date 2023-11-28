cf1f0add7eb4e6972dc36337e557769e446e832e
Jorlyna Pujols <jorlynapujols@Jorlynas-MBP.attlocal.net>
2023-11-27 21:34:12 -0500

    updated index db updated put db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log("Post to the database");
+
+  // Creates a connection to the jate database and version.
+  const jateDb = await openDB("jate", 1);
+
+  // Creates a new transaction and specifies the database and data privileges.
+  const tx = jateDb.transaction("jate", "readwrite");
+
+  // Opens up the desired object store.
+  const store = tx.objectStore("jate");
+
+  // Uses the .put() method on the store and passes in the content.
+  const request = store.put({ id: 1, value: content });
+
+  // Gets confirmation of the request.
+  const result = await request;
+  console.log("Data saved to the database:", result.values);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
