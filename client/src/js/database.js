157e8aa1f0b094e3a6199a6b4a6a00350a65b89f
Jorlyna Pujols <jorlynapujols@Jorlynas-MBP.attlocal.net>
2023-11-27 21:38:01 -0500

    completed PWA

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,8 +14,6 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log("Post to the database");
-
   // Creates a connection to the jate database and version.
   const jateDb = await openDB("jate", 1);
 
@@ -30,10 +28,33 @@ export const putDb = async (content) => {
 
   // Gets confirmation of the request.
   const result = await request;
-  console.log("Data saved to the database:", result.values);
+  console.log("Data saved to the database:", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+
+// Exports a function to get the database.
+export const getDb = async () => {
+  // Creates a connection to the jate database and version.
+  const jateDb = await openDB("jate", 1);
+
+  // Creates a new transaction and specifies the database and data privileges.
+  const tx = jateDb.transaction("jate", "readonly");
+
+  // Opens up the desired object store.
+  const store = tx.objectStore("jate");
+
+  // Uses the .get(1) method to retrieve the value of the first record matching the query.
+
+  const request = store.get(1);
+
+  // Gets confirmation of the request.
+  const result = await request;
+  result
+    ? console.log(" retrieved from database:", result.value)
+    : console.log("Nothing found in database!");
+  return result?.value;
+};
 
 initdb();
